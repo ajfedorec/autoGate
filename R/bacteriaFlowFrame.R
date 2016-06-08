bcdf171f51c1ac221d48b49797c6f42cc337084e
A J H Fedorec <ajfedorec@gmail.com>
2016-06-08 13:17:22 +0100

    Changed tab to 2 spaces from 4

--- R/bacteriaFlowFrame.R
+++ R/bacteriaFlowFrame.R
@@ -1,25 +1,25 @@
 get.bacteria <- function(flow_frame) {
-    ## This adjusts the number of clusters to fit based on if there might be a
-    ## cluster for background debris.
-    ## If the FACS data has been thresholded below 8000 we see background junk
-    ## forming a second cluster.
-    ## n.b. the 8000 figure is likely to be changeable, maybe for each run?
-    ## n.b. in some data we see 2 clusters of bacteria, so 3 clusters. This
-    ##      could cause problems.
-    if (summary(flow_frame)["Min.", "FSC-H"] <= log10(8000)){
-        K_clusters <- 2
-    }   else {
-        K_clusters <- 1; #####CHANGEME: 2 HARDCODED VALUES #####
-    }
+  ## This adjusts the number of clusters to fit based on if there might be a
+  ## cluster for background debris.
+  ## If the FACS data has been thresholded below 8000 we see background junk
+  ## forming a second cluster.
+  ## n.b. the 8000 figure is likely to be changeable, maybe for each run?
+  ## n.b. in some data we see 2 clusters of bacteria, so 3 clusters. This
+  ##      could cause problems.
+  if (summary(flow_frame)["Min.", "FSC-H"] <= log10(8000)){
+    K_clusters <- 2
+  }   else {
+    K_clusters <- 1; #####CHANGEME: 2 HARDCODED VALUES #####
+  }
 
-    ## remove outliers based on forward and side scatter
-    ## NOT SURE IF 90% or 95% is better
-    ##  This fits a cluster (or 2 clusters) to the data and removes 95% outliers
-    res95 <- flowClust(flow_frame,
-                       varNames = c("FSC-H", "SSC-H"),
-                       K = K_clusters,
-                       level = 0.95) #####CHANGEME: HARDCODED VALUE #####
-    bact_flow_frame <- split(flow_frame, res95)[[which.max(res95@mu[, 1])]]
+  ## remove outliers based on forward and side scatter
+  ## NOT SURE IF 90% or 95% is better
+  ##  This fits a cluster (or 2 clusters) to the data and removes 95% outliers
+  res95 <- flowClust(flow_frame,
+                     varNames = c("FSC-H", "SSC-H"),
+                     K = K_clusters,
+                     level = 0.95) #####CHANGEME: HARDCODED VALUE #####
+  bact_flow_frame <- split(flow_frame, res95)[[which.max(res95@mu[, 1])]]
 
-    return(bact_flow_frame)
+  return(bact_flow_frame)
 }
--- R/singletsFlowFrame.R
+++ R/singletsFlowFrame.R
@@ -1,10 +1,10 @@
 get.singlets <- function(flow_frame){
-    ## fit a line to SSC-H and SSC-A
-    lm_fit <- lm(flow_frame[, "SSC-H"]@exprs ~ flow_frame[, "SSC-A"]@exprs)
+  ## fit a line to SSC-H and SSC-A
+  lm_fit <- lm(flow_frame[, "SSC-H"]@exprs ~ flow_frame[, "SSC-A"]@exprs)
 
-    ## trim points that fall "too far" from the line
-    ##      This is an attempt to remove doublets and other cell clumps.
-    singlet_flow_frame <- Subset(flow_frame, lm_fit$residuals ^ 2 < 0.05) #####CHANGEME: HARDCODED VALUE #####
+  ## trim points that fall "too far" from the line
+  ##      This is an attempt to remove doublets and other cell clumps.
+  singlet_flow_frame <- Subset(flow_frame, lm_fit$residuals ^ 2 < 0.05) #####CHANGEME: HARDCODED VALUE #####
 
-    return(singlet_flow_frame)
+  return(singlet_flow_frame)
 }
